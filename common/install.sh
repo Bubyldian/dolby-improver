0a2a95282d22e1385f89d7e3854587508f55f7c2
Bubyldian <97834417+Bubyldian@users.noreply.github.com>
2024-05-29 13:16:40 +0200

    cleanup

--- common/install.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /data/vendor/dolby
-if [ -d "/data/adb/modules/MarbleDax" ]; then
-	ui_print "- The old version of the module will be removed!"
-    touch "/data/adb/modules/MarbleDax/remove"
-fi
